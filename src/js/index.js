c9e3bbefa1817010c37ab25e4d1d79253ab0cb1e
Jacqueline Nunes <115042952+JacqueNunes@users.noreply.github.com>
2023-03-15 10:07:51 -0300

    add interval in js

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,34 @@
-const botoesCarrossel = document.querySelectorAll('.botao');
-const imagens = document.querySelectorAll('.imagem');
+let imagens = document.querySelectorAll('.carrossel img');
+let indice = 0;
 
-botoesCarrossel.forEach((botao, indice) => {
-	botao.addEventListener('click', () => {
-		const imagemAtual = document.querySelector('.ativa');
-		const botaoAtual = document.querySelector('.selecionado');
 
-		botaoAtual.classList.remove('selecionado');
-		imagemAtual.classList.remove('ativa');
+setInterval(() => {
+    imagens[indice].classList.remove('ativa');
+    indice = (indice + 1) % imagens.length;
+    imagens[indice].classList.add('ativa');
+	
+}, 5000);
+
+
+
+
+
+
+// const botoesCarrossel = document.querySelectorAll('.botao');
+// const imagens = document.querySelectorAll('.imagem');
+
+// botoesCarrossel.forEach((botao, indice) => {
+// 	botao.addEventListener('click', () => {
+// 		const imagemAtual = document.querySelector('.ativa');
+// 		const botaoAtual = document.querySelector('.selecionado');
+
+// 		botaoAtual.classList.remove('selecionado');
+// 		imagemAtual.classList.remove('ativa');
         
-		imagens[indice].classList.add('ativa');
-		botoesCarrossel[indice].classList.add('selecionado');
-	});
-});
+// 		imagens[indice].classList.add('ativa');
+// 		botoesCarrossel[indice].classList.add('selecionado');
+// 	});
+// });
 
 
   
